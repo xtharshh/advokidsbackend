73ade7aeefc84c32d49192c335db75c181612262
HARSH KUMAR <xt.harshh@gmail.com>
2025-08-10 08:56:13 +0530

    advokids backend

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');  
+const jwt = require('jsonwebtoken');
 const User = require('../models/model');
-
 const app = express();
 app.use(express.json());
 app.use(require('cors')({ origin: '*' }));
